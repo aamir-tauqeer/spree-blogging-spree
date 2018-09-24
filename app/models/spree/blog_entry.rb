16f0b3771f57549ed6a4e82298c595a43b67b6d7
Waqas <mwaqas.shah@gmail.com>
2018-09-25 01:52:40 +0500

    Support for spree 3.6 and rails 5

--- app/models/spree/blog_entry.rb
+++ app/models/spree/blog_entry.rb
@@ -17,8 +17,7 @@ class Spree::BlogEntry < ActiveRecord::Base
     belongs_to :author
   end
 
-  has_one :blog_entry_image, :as => :viewable, :dependent => :destroy, :class_name => 'Spree::BlogEntryImage'
-  accepts_nested_attributes_for :blog_entry_image, :reject_if => :all_blank
+  has_one_attached :image
 
   def entry_summary(chars=200)
     if summary.blank?
